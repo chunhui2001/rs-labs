defa4a35ff31e354424e5a43c306d56706486c5c
zhangchunhui <chunhui2001@gmail.com>
2023-01-01 11:19:26 +0800

    a
    a

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,19 +1,19 @@
-struct MyStruct<'a> {
-    x: &'a i32,
-    y: &'a i32,
+fn extend_slice(vec: &mut Vec<f64>, slice: &[f64]) {
+    for elt in slice {
+        vec.push(*elt);
+    }
 }
 
 fn main() {
-    let x = 10;
-    let r;
+    let mut wave = Vec::new();
+    let head = vec![0.0, 1.0];
+    let tail = [1.2, 0.34];
 
-    {
-        let y = 20;
-        {
-            let s = MyStruct { x: &x, y: &y };
-            r = s.x;
-            println!("{}, {}", s.x, s.y);
-        }
-        println!("{}", r);
-    }
+    extend_slice(&mut wave, &head); // 用另一个向量扩展 wave
+    extend_slice(&mut wave, &tail); // 用数组扩展 wave
+
+    wave.push(1.0);
+
+    // extend_slice(&mut wave, &wave)
+    println!("{:?}, {}, {}", wave, wave.len(), wave.capacity());
 }
